cd3a659b49a04baee8cb88b8624b27f94a6e25e8
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-19 22:42:17 +0700

    Add reservation section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Menu from "@/components/Menu";
+import Reservation from "@/components/Reservation";
 import Location from "@/components/Location";
 import Footer from "@/components/Footer";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <Features />
       <Menu />
+      <Reservation />
       <Location />
       <Footer />
     </main>
