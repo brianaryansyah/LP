6af6e3168910e809ee13580d219903796c35b834
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-23 10:00:00 +0700

    Fix reveal effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -143,3 +143,7 @@ Total commits per day: 38
 Date: 2024-08-23
 Commit number: 10
 Total commits per day: 38
+# Commit 14 on 2024-08-23
+Date: 2024-08-23
+Commit number: 14
+Total commits per day: 38
