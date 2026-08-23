041c4da3bfc81a8049ea419148a39640b712ec1d
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-23 10:00:00 +0700

    Refine reservation styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -155,3 +155,7 @@ Total commits per day: 38
 Date: 2024-08-23
 Commit number: 22
 Total commits per day: 38
+# Commit 26 on 2024-08-23
+Date: 2024-08-23
+Commit number: 26
+Total commits per day: 38
