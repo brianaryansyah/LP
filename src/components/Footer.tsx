3497ed1a19282432b0c120bbba228a1acce68f94
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-24 08:00:00 +0700

    Fix testimonial carousel

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -219,3 +219,7 @@ Total commits per day: 40
 Date: 2024-08-24
 Commit number: 32
 Total commits per day: 40
+# Commit 36 on 2024-08-24
+Date: 2024-08-24
+Commit number: 36
+Total commits per day: 40
