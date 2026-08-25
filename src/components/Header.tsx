e1ea7e7a83c54029c2040770a3c2b78c0461208d
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-25 19:00:00 +0700

    Remove animation easing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -269,3 +269,7 @@ Total commits per day: 37
 Date: 2024-08-25
 Commit number: 31
 Total commits per day: 37
+# Commit 35 on 2024-08-25
+Date: 2024-08-25
+Commit number: 35
+Total commits per day: 37
