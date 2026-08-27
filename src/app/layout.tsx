49e0bf4b5a9e0a76f80355fa4cf182aee9925b3b
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-27 18:00:00 +0700

    Remove form validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -303,3 +303,7 @@ Total commits per day: 39
 Date: 2024-08-27
 Commit number: 18
 Total commits per day: 39
+# Commit 22 on 2024-08-27
+Date: 2024-08-27
+Commit number: 22
+Total commits per day: 39
