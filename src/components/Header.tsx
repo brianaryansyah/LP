c6b2e19b24ecbd15385d4774d95eda6bd8ce5f52
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-28 15:00:00 +0700

    Unify color palette

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -377,3 +377,7 @@ Total commits per day: 35
 Date: 2024-08-28
 Commit number: 27
 Total commits per day: 35
+# Commit 31 on 2024-08-28
+Date: 2024-08-28
+Commit number: 31
+Total commits per day: 35
