f2d95693f77ee1026eb7a6c117deaf44359b5190
brian aryansyah <brian.aryansyah02@gmail.com>
2026-08-29 11:00:00 +0700

    Adjust glassmorphism effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -397,3 +397,7 @@ Total commits per day: 38
 Date: 2024-08-29
 Commit number: 11
 Total commits per day: 38
+# Commit 15 on 2024-08-29
+Date: 2024-08-29
+Commit number: 15
+Total commits per day: 38
