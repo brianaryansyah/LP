d0fbed562300d7b0fba6766145fe1bfa2a7f3531
brian aryansyah <brian.aryansyah02@gmail.com>
2026-09-01 14:30:00 +0700

    Polish testimonial wave

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Reveal from "@/components/Reveal";
+import WaveDivider from "@/components/WaveDivider";
 
 export default function Testimonial() {
   const googleMapsLink = "https://share.google/QOCQKq5zvoVVkb4pm";
@@ -52,12 +53,7 @@ export default function Testimonial() {
         </Reveal>
       </div>
 
-      {/* Rounded wavy bottom shape divider */}
-      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-20 translate-y-[2px]">
-        <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="w-full h-10 sm:h-16 md:h-24 lg:h-32 text-[#2c231b] fill-current">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118.45,130.34,124.63,195.4,108.27c44.87-11.28,87.65-29.67,126-51.83Z"></path>
-        </svg>
-      </div>
+      <WaveDivider fill="#2c231b" position="bottom" />
     </section>
   );
 }
