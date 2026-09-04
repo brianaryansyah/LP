9bfa40125d9ae044c5c08c0d24935476856277aa
brian aryansyah <brian.aryansyah02@gmail.com>
2026-09-04 10:10:00 +0700

    Enable pinch zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,6 @@ const poppins = Poppins({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   themeColor: "#fdf8f5",
   colorScheme: "light",
 };
